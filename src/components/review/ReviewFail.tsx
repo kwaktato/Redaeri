62e0dfa654d5b4af02c29ca5d96517debd6d8318
kwaktato <109459319+kwaktato@users.noreply.github.com>
2025-02-27 15:22:00 +0900

    feat: 답변 작성 실패시 ui

---
+++ src/components/review/ReviewFail.tsx
@@ -0,0 +1,74 @@
+import Warning from '@/assets/images/warning.svg?react';
+import { useNavigate } from 'react-router';
+import styled from 'styled-components';
+
+// 5.4 답변 작성 실패 시
+const ReviewFail = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <Wrapper>
+        <Warning />
+        <Label>
+          리대리가 답변 작성을 어려워하고 있어요.
+          <br />
+          잠시 후 다시 시도해 주세요.
+        </Label>
+      </Wrapper>
+      <Button
+        onClick={() => {
+          navigate('/review');
+        }}
+      >
+        확인
+      </Button>
+    </Container>
+  );
+};
+
+export default ReviewFail;
+
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+`;
+
+const Label = styled.div`
+  color: ${({ theme }) => theme.colors['neutral-600']};
+  text-align: center;
+  font-family: 'GmarketSansMedium';
+  font-size: 22px;
+  line-height: 30px;
+`;
+
+const Button = styled.button`
+  position: absolute;
+  left: 32px;
+  right: 32px;
+  bottom: 48px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 58px;
+  border-radius: 12px;
+  background: ${({ theme }) => theme.colors['neutral-600']};
+
+  color: ${({ theme }) => theme.colors.white};
+  font-family: 'Pretendard Variable';
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 30px;
+`;
