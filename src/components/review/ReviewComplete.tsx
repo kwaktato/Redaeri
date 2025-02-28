8b3416c59bf0ba44c4b6e26f2b86962914ea941b
kwaktato <happyrhkr@g.hongik.ac.kr>
2025-02-28 20:49:50 +0000

    deploy: Link 대신 button으로 변경

--- src/components/review/ReviewComplete.tsx
+++ src/components/review/ReviewComplete.tsx
@@ -5,16 +5,26 @@ import CopyIcon from '@/assets/images/copy.svg?react';
 import Tooltip from '@/assets/images/tooltip.png';
 import Star from './Star';
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router';
+import axios from 'axios';
+import { useNavigate } from 'react-router';
+
+interface ReviewCompleteProps {
+  anotherButton: () => void;
+  postReview: () => void;
+}
 
 // 5.4 답변 완료 - 완료
-const ReviewComplete = () => {
+/* eslint-disable no-console */
+const ReviewComplete = ({ anotherButton, postReview }: ReviewCompleteProps) => {
   const [score, setScore] = useState(0);
-
   const getReview = () => {
-    // api 호출
+    // score도 전달인자로 받아야 함!
     setScore(4);
   };
+
+  const navigate = useNavigate();
+
+  // const [name, setName] = useState('');
 
   // 답변 복사하기
   const copy = async () => {
@@ -28,8 +38,29 @@ const ReviewComplete = () => {
     // }
   };
 
+  // api 연결 - 베이커리 이름 가져오기
+  const baseURL = import.meta.env.VITE_APP_API_URL;
+  const getName = async () => {
+    // const token = localStorage.getItem('token');
+    const token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dpbklkeCI6MjUsImV4cCI6MTc0MDgzNTQyNSwiaWF0IjoxNzQwNzQ5MDI1fQ.LLZ4UrDZ27-Kd8RNEfOmGAFjgzyXFA-Jw2ufUSv-3a0';
+    try {
+      const result = await axios.get(`${baseURL}/api/v1/user/get`, {
+        headers: {
+          'Content-Type': 'applicatoin/json',
+          Token: token,
+        },
+      });
+      // setName(result.data.storeName);
+      console.log(result.data);
+    } catch (e) {
+      console.log('가게 이름 가져오기 에러: ', e);
+    }
+  };
+
   useEffect(() => {
     getReview();
+    getName();
   }, []);
 
   return (
@@ -40,6 +71,7 @@ const ReviewComplete = () => {
 
       <TitleWrapper>
         <TitleDetail>
+          {/* <span>{name}</span>에 대한 */}
           <span>웨일즈 베이커리</span>에 대한
         </TitleDetail>
         <Title>
@@ -67,9 +99,12 @@ const ReviewComplete = () => {
       <Arrow />
 
       <AnswerWrapper>
-        <AnswerTitle>
-          이렇게 <span>답변해보시는 건 어떨까요?</span>
-        </AnswerTitle>
+        <AnswerTitleWrapper>
+          <AnswerTitle>
+            이렇게 <span>답변해보시는 건 어떨까요?</span>
+          </AnswerTitle>
+          <Copy onClick={copy} />
+        </AnswerTitleWrapper>
         <Answer>
           저희 가게 음식을 좋아해주셔서 감사합니다저희 가게 음식을 좋아해주셔서
           감사합니다저희 가게 음식을 좋아해주셔서 감사합니다저희 가게 음식을
@@ -77,9 +112,8 @@ const ReviewComplete = () => {
           가게 음식을 좋아해주셔서 감사합니다저희 가게 음식을 좋아해주셔서
           감사합니다저희 가게 음식을 좋아해주셔서 감사합니다저희 가게 음식을
           좋아해주셔서 감사합니다저희 가
-          <Copy onClick={copy} />
         </Answer>
-        <Rewrite>
+        <Rewrite onClick={postReview}>
           원하는 답변이 아닌가요? <span>답변 재작성하기</span>
         </Rewrite>
       </AnswerWrapper>
@@ -88,10 +122,16 @@ const ReviewComplete = () => {
         <Border />
         <ButtonWrapper>
           <img src={Tooltip} alt='tooltip' />
-          <Button state='' to='upload-review'>
+          <Button state='' onClick={() => navigate('/upload-review')}>
             스타일 수정하기
           </Button>
-          <Button state='black' to='/review'>
+          <Button
+            state='black'
+            onClick={() => {
+              navigate('/review');
+              anotherButton();
+            }}
+          >
             다른 리뷰도 답변하기
           </Button>
         </ButtonWrapper>
@@ -204,6 +244,12 @@ const AnswerWrapper = styled.div`
   margin-bottom: 36px;
 `;
 
+const AnswerTitleWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
 const AnswerTitle = styled.label`
   color: ${({ theme }) => theme.colors.white};
   font-family: 'Pretendard Variable';
@@ -217,7 +263,7 @@ const AnswerTitle = styled.label`
 `;
 
 const Answer = styled.label`
-  padding: 12px 52px 12px 16px;
+  padding: 12px 16px;
   border-radius: 12px;
   background: ${({ theme }) => theme.colors['primary-100']};
   border: 1px solid ${({ theme }) => theme.colors['primary-200']};
@@ -226,13 +272,10 @@ const Answer = styled.label`
   font-size: 16px;
   font-weight: 500;
   line-height: 162%;
-  position: relative;
 `;
 
 const Copy = styled(CopyIcon)`
-  position: absolute;
-  top: 12px;
-  right: 12px;
+  width: 32px;
 `;
 
 const Rewrite = styled.button`
@@ -277,7 +320,7 @@ const ButtonWrapper = styled.div`
   }
 `;
 
-const Button = styled(Link)<{ state: string }>`
+const Button = styled.button<{ state: string }>`
   display: flex;
   width: 100%;
   height: 52px;
