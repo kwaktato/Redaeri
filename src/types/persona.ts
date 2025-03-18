2296730c9095e27d4d86fe054a5f5e8c3a22ffe1
kwaktato <happyrhkr@g.hongik.ac.kr>
2025-03-18 05:19:31 +0000

    fix: 오타 수정

--- src/types/persona.ts
+++ src/types/persona.ts
@@ -17,7 +17,7 @@ export const personaMapping: { [key: string]: string } = {
   '예의 바르고 나이스한 30대 초보 사장님': 'nicePersona',
   '단골 챙기는 정 많고 유쾌한 40대 사장님': 'pleasantPersona',
   '묵묵히 음식에 최선을 다하는 60대 사장님': 'silentPersona',
-  '충청도 출식 외식업 강자 사장님': 'chungcheongdoPersona',
+  '충청도 출신 외식업 강자 사장님': 'chungcheongdoPersona',
 };
 
 export const emotionMapping: { [key: string]: string } = {
@@ -57,7 +57,7 @@ export const PERSONA_SELECT_PERSONA_KEY = [
   '예의 바르고 나이스한 30대 초보 사장님',
   '단골 챙기는 정 많고 유쾌한 40대 사장님',
   '묵묵히 음식에 최선을 다하는 60대 사장님',
-  '충청도 출식 외식업 강자 사장님',
+  '충청도 출신 외식업 강자 사장님',
 ];
 
 export const PERSONA_EMOTION_QUESTION = [
@@ -69,7 +69,7 @@ export const PERSONA_EMOTION_QUESTION = [
 export const PERSONA_LENGTH_QUESTION = [
   '정성이 최고 긴~ 답변',
   '길지도 짧지도 않게 알잘딱깔센',
-  '정성이 최고 짧은~ 답변',
+  '짧고 핵심만 간단하게! 단문 답변',
 ];
 
 export interface PersonaInsertType {
